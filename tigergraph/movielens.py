7e60c790c1d47bb16ebc47ad0b61bdbb8f66d700
Nino Brezac <nino.brezac@gmail.com>
2024-09-21 02:16:10 +0200

    TigerGraph import

--- tigergraph/movielens.py
+++ tigergraph/movielens.py
@@ -1,46 +1,10 @@
 import pyTigerGraph as tg
-import pandas as pd
+import time
 
 class TigerGraphCommands:
     def __init__(self, host, graphname, username, password):
         self.conn = tg.TigerGraphConnection(host=host, graphname=graphname, username=username, password=password)
     
-    def getSchema(self):
-        return self.conn.getSchema()
-
-    def insert_vertex(self, vertex_type, vertex_id, properties):
-        # Insert vertex, passing the primary ID (vertex_id) separately
-        self.conn.upsertVertex(vertex_type, vertex_id, properties)
-
-    def insert_edge(self, from_vertex_id, to_vertex_id, edge_type, properties=None):
-        if properties is None:
-            properties = {}
-        # Insert edge between two vertices
-        self.conn.upsertEdge("User", from_vertex_id, edge_type, "User", to_vertex_id, properties)
-
-    def find_vertex(self, vertex_type, vertex_id=None):
-        # If vertex_id is provided, use it to get a specific vertex
-        if vertex_id:
-            return self.conn.getVerticesById(vertex_type, vertex_id)
-        # Otherwise, return all vertices of the given type
-        return self.conn.getVertices(vertex_type)
-
-    def find_edge(self, from_vertex_id, to_vertex_id, edge_type):
-        # Get edges between two vertices
-        return self.conn.getEdges("User", from_vertex_id, edge_type)
-
-    def update_vertex(self, vertex_type, vertex_id, update_properties):
-        # Update the vertex with new properties
-        self.conn.upsertVertex(vertex_type, vertex_id, update_properties)
-
-    def delete_vertex(self, vertex_type, vertex_id):
-        # Delete the vertex by ID
-        self.conn.deleteVertices(vertex_type, where="PRIMARY_ID = '" + vertex_id + "'")
-
-    def delete_edge(self, from_vertex_id, to_vertex_id, edge_type):
-        # Delete the edge between two vertices
-        self.conn.deleteEdges("User", from_vertex_id, edge_type, "User", to_vertex_id)
-
     def load_movies(self, movies_csv_path):
         # Define the loading job for movies
         self.conn.gsql("USE GRAPH movielens DROP JOB load_movies")
@@ -48,81 +12,104 @@ class TigerGraphCommands:
         USE GRAPH movielens
         CREATE LOADING JOB load_movies FOR GRAPH {self.conn.graphname} {{
             DEFINE FILENAME movies_file="{movies_csv_path}";
-            LOAD movies_file TO VERTEX Movie VALUES ($0, $1, $2) USING SEPARATOR=",st";
+            LOAD movies_file TO VERTEX Movie VALUES ($0, $1, $2) USING SEPARATOR="%";
         }}"""
         # Create and run the loading job for movies
         self.conn.gsql(movie_loading_job)
         self.conn.gsql(f'USE GRAPH movielens RUN LOADING JOB load_movies')
         print(f"Movies loaded from {movies_csv_path}.")
     
-    def load_movie_genre_edges(self, movies_csv_path):
-        # Load genres from movies.csv
-        movies_df = pd.read_csv(movies_csv_path)
-        
-        # Insert HAS_GENRE edges between movies and genres
-        for _, row in movies_df.iterrows():
-            movie_id = row['movieId']
-            genres = row['genres'].split('|')
-            for genre in genres:
-                self.conn.upsertEdge("Movie", movie_id, "HAS_GENRE", "Genre", genre)
-        
-        print(f"Movie-genre edges loaded from {movies_csv_path}.")
-    
+    def load_genres(self, genres_csv_path):
+        # Define the loading job for genres
+        self.conn.gsql("USE GRAPH movielens DROP JOB load_genres")
+        genres_loading_job = f"""
+        USE GRAPH movielens
+        CREATE LOADING JOB load_genres FOR GRAPH {self.conn.graphname} {{
+            DEFINE FILENAME genres_file="{genres_csv_path}";
+            LOAD genres_file TO VERTEX Genre VALUES ($0, $1) USING SEPARATOR=",";
+        }}"""
+        # Create and run the loading job for genres
+        self.conn.gsql(genres_loading_job)
+        self.conn.gsql(f'USE GRAPH movielens RUN LOADING JOB load_genres')
+        print(f"Genres loaded from {genres_csv_path}.")
+
     def load_users(self, users_csv_path):
-        # Load users from users.csv (preprocessed from both ratings.csv and tags.csv)
-        users_df = pd.read_csv(users_csv_path, header=None, names=['userId'])
-        
-        # Insert users
-        for _, row in users_df.iterrows():
-            user_id = row['userId']
-            self.insert_vertex("User", user_id, {})
-        
+        # Define the loading job for users
+        self.conn.gsql("USE GRAPH movielens DROP JOB load_users")
+        users_loading_job = f"""
+        USE GRAPH movielens
+        CREATE LOADING JOB load_users FOR GRAPH {self.conn.graphname} {{
+            DEFINE FILENAME users_file="{users_csv_path}";
+            LOAD users_file TO VERTEX User VALUES ($0) USING SEPARATOR="%";
+        }}"""
+        # Create and run the loading job for users
+        self.conn.gsql(users_loading_job)
+        self.conn.gsql(f'USE GRAPH movielens RUN LOADING JOB load_users')
         print(f"Users loaded from {users_csv_path}.")
     
     def load_ratings(self, ratings_csv_path):
-        # Load ratings from ratings.csv
-        ratings_df = pd.read_csv(ratings_csv_path)
-        
-        # Insert RATED edges
-        for _, row in ratings_df.iterrows():
-            user_id = row['userId']
-            movie_id = row['movieId']
-            rating = row['rating']
-            self.conn.upsertEdge("User", user_id, "RATED", "Movie", movie_id, {"rating": rating})
-        
+        # Define the loading job for ratings
+        self.conn.gsql("USE GRAPH movielens DROP JOB load_ratings")
+        ratings_loading_job = f"""
+        USE GRAPH movielens
+        CREATE LOADING JOB load_ratings FOR GRAPH {self.conn.graphname} {{
+            DEFINE FILENAME ratings_file="{ratings_csv_path}";
+            LOAD ratings_file TO EDGE RATED VALUES ($0, $1, $2, $3) USING SEPARATOR="|";
+        }}"""
+        # Create and run the loading job for ratings
+        self.conn.gsql(ratings_loading_job)
+        self.conn.gsql(f'USE GRAPH movielens RUN LOADING JOB load_ratings')
         print(f"Ratings loaded from {ratings_csv_path}.")
     
     def load_tags(self, tags_csv_path):
-        # Load tags from tags.csv
-        tags_df = pd.read_csv(tags_csv_path)
-        
-        # Insert TAGGED edges
-        for _, row in tags_df.iterrows():
-            user_id = row['userId']
-            movie_id = row['movieId']
-            tag = row['tag']
-            self.conn.upsertEdge("User", user_id, "TAGGED", "Movie", movie_id, {"tag": tag})
-        
+        # Define the loading job for tags
+        self.conn.gsql("USE GRAPH movielens DROP JOB load_tags")
+        tags_loading_job = f"""
+        USE GRAPH movielens
+        CREATE LOADING JOB load_tags FOR GRAPH {self.conn.graphname} {{
+            DEFINE FILENAME tags_file="{tags_csv_path}";
+            LOAD tags_file TO EDGE TAGGED VALUES ($0, $1, $2, $3) USING SEPARATOR="|";
+        }}"""
+        # Create and run the loading job for tags
+        self.conn.gsql(tags_loading_job)
+        self.conn.gsql(f'USE GRAPH movielens RUN LOADING JOB load_tags')
         print(f"Tags loaded from {tags_csv_path}.")
-        
+    
+    def load_movie_genre_edges(self, movie_genre_edges_csv_path):
+        # Define the loading job for movie-genre edges
+        self.conn.gsql("USE GRAPH movielens DROP JOB load_movie_genre_edges")
+        movie_genre_edges_job = f"""
+        USE GRAPH movielens
+        CREATE LOADING JOB load_movie_genre_edges FOR GRAPH {self.conn.graphname} {{
+            DEFINE FILENAME movie_genre_file="{movie_genre_edges_csv_path}";
+            LOAD movie_genre_file TO EDGE HAS_GENRE VALUES ($0, $1) USING SEPARATOR=",";
+        }}"""
+        # Create and run the loading job for movie-genre edges
+        self.conn.gsql(movie_genre_edges_job)
+        self.conn.gsql(f'USE GRAPH movielens RUN LOADING JOB load_movie_genre_edges')
+        print(f"Movie-genre edges loaded from {movie_genre_edges_csv_path}.")
 
 # Initialize TigerGraphCommands with your TigerGraph instance details
 tigergraph_client = TigerGraphCommands(host="http://localhost", graphname="movielens", username="tigergraph", password="tigergraph")
 
-# Check schema
-#results = TigerGraphCommands.getSchema(self=tigergraph_client)
-#print(results)
+# Paths to CSV files
+movies_csv_path = r"/tmp/movies.csv"
+genres_csv_path = r"/tmp/genres.csv"
+users_csv_path = r"/tmp/users.csv"
+ratings_csv_path = r"/tmp/ratings.csv"
+tags_csv_path = r"/tmp/tags.csv"
+movie_genre_edges_csv_path = r"/tmp/movie_genre_edges.csv"
 
-movies_csv_path = r"../datasets/ml-32m/movies.csv"
-genres_csv_path = r"../datasets/ml-32m/genres.csv"
-users_csv_path = r"../datasets/ml-32m/users.csv"
-ratings_csv_path = r"../datasets/ml-32m/ratings.csv"
-tags_csv_path = r"../datasets/ml-32m/tags.csv"
 
-# Load movies, genres, users, ratings, and tags
-# tigergraph_client.load_movies(movies_csv_path)
-# tigergraph_client.load_genres(genres_csv_path)
-tigergraph_client.load_movie_genre_edges(movies_csv_path)
-# tigergraph_client.load_users(users_csv_path)
-# tigergraph_client.load_ratings(ratings_csv_path)
-# tigergraph_client.load_tags(tags_csv_path)
+# Call individual loading functions
+total_start_time = time.time()
+tigergraph_client.load_movies(movies_csv_path)
+tigergraph_client.load_genres(genres_csv_path)
+tigergraph_client.load_users(users_csv_path)
+tigergraph_client.load_tags(tags_csv_path)
+tigergraph_client.load_movie_genre_edges(movie_genre_edges_csv_path)
+tigergraph_client.load_ratings(ratings_csv_path)
+total_end_time = time.time()
+total_elapsed_time = total_end_time - total_start_time
+
+print(f"Data imported successfully! Total time: {total_elapsed_time:.2f} seconds.")
--- tigergraph/movielens.sh
+++ tigergraph/movielens.sh
@@ -8,24 +8,25 @@ docker exec tigergraph //home/tigergraph/tigergraph/app/4.1.0/cmd/gsql "CREATE U
 docker exec tigergraph //home/tigergraph/tigergraph/app/4.1.0/cmd/gsql "CREATE GRAPH movielens(User, Movie, Genre, RATED, TAGGED, HAS_GENRE)"
 docker exec tigergraph //home/tigergraph/tigergraph/app/4.1.0/cmd/gsql "INSTALL DATASET"
 
-docker cp ../datasets/ml-32m/movies.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/movies.csv
-docker cp ../datasets/ml-32m/tags.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/tags.csv
-docker cp ../datasets/ml-32m/ratings.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/ratings.csv
-docker cp ../datasets/ml-32m/genres.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/genres.csv
-docker cp ../datasets/ml-32m/users.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/users.csv
+# docker cp ../datasets/ml-32m/movies.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/movies.csv
+# docker cp ../datasets/ml-32m/tags.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/tags.csv
+# docker cp ../datasets/ml-32m/ratings.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/ratings.csv
+# docker cp ../datasets/ml-32m/genres.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/genres.csv
+# docker cp ../datasets/ml-32m/users.csv tigergraph:/home/tigergraph/tigergraph/data/gsql/users.csv
 
 docker exec tigergraph //home/tigergraph/tigergraph/app/4.1.0/cmd/gsql "SHOW GRAPH movielens"
 docker exec -it tigergraph //home/tigergraph/tigergraph/app/4.1.0/cmd/gadmin "restart gsql"
 
-
-INSTALL QUERY tg_louvain
-INSTALL QUERY tg_betweenness_cent
-# running algorithms
+# Running algorithms
 #docker exec -tigergraph sh
 #//home/tigergraph/tigergraph/app/4.1.0/cmd//gsql SET query_timeout=60
 # gsql
 # USE GRAPH movielens
 # RUN QUERY tg_betweenness_cent 
+INSTALL QUERY tg_louvain
+INSTALL QUERY tg_betweenness_cent
+INTERPRET QUERY (/* Parameters here */) FOR GRAPH movielenssmall { 
+  all = {ANY};
 
 # Deleting everything
 #DROP USERS
@@ -34,3 +35,30 @@ INSTALL QUERY tg_betweenness_cent
 #DROP ALL QUERIES
 #DROP ALL JOBS
 # CLEAR GRAPH STORE -HARD
+INTERPRET QUERY () FOR GRAPH movielens { 
+  all = {ANY};
+  results = SELECT a FROM all:a - (:e) - ANY
+  ACCUM
+    DELETE(e)
+  POST-ACCUM
+    DELETE(a);
+}
+
+docker exec tigergraph gsql "USE GRAPH movielens
+DROP VERTEX Movie;
+CREATE VERTEX Movie (PRIMARY_ID id UINT, title STRING, genres STRING);"
+
+docker exec -it  -u 0 tigergraph sh
+apt-get update
+apt-get install nano
+nano ~/.bashrc
+export PATH=$PATH:/home/tigergraph/tigergraph/app/4.1.0/cmd
+. ~/.bashrc
+
+gsql
+use global CREATE GLOBAL SCHEMA_CHANGE JOB add_genres_to_movie {ALTER VERTEX Movie ADD ATTRIBUTE (gen
+res STRING);}
+RUN GLOBAL SCHEMA_CHANGE JOB add_genres_to_movie
+
+use global CREATE GLOBAL SCHEMA_CHANGE JOB fix_genres {ALTER VERTEX Genre ADD ATTRIBUTE (name STRING);}
+run global schema_change job fix_genres
